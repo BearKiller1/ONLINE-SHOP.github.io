69b4e59c3955699e68c448d900f203f96c8ccfd3
UniqueDesigns-UI <dachi.xucishvili8@gmail.com>
2020-09-28 15:31:16 +0400

    TEST#CATALOG ADD PRODUCT

--- test.php
+++ test.php
@@ -18,7 +18,7 @@
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-      <link rel="stylesheet" href="style/test_style.css">
+      <link rel="stylesheet" href="style/test.css">
       <link rel="stylesheet" href="nightmode.js">
     <!-- <link rel="stylesheet" href="Js/nightmode.js"> -->
   </head>
@@ -27,9 +27,11 @@
         <div class="gallery js-flickity"
           data-flickity-options='{ "wrapAround": true ,"pageDots": false,"autoPlay": 1500 }'>
           <?php
-            $x = 15;
+            $x = 5;
             for ($i=0; $i < $x; $i++) {
-              echo "<div class='gallery-cell'></div>";
+              echo "<div class='gallery-cell'>
+                <img src='source/d".$i.".jpg'>
+              </div>";
               if($i == $x-1){
                 echo "<div class='add_prod_to_catalog'>
                   <button id='open_image'>+</button>
@@ -39,17 +41,32 @@
           ?>
         </div>
       </div>
-
-      <div class="prod_list_to_add">
-        <div class="tmp_image">
-          <img src="source/d1.jpg" alt="bad">
-        </div>
+      <div class='black_div'>
       </div>
-
+      <div class="prod_list_to_add">
+            <button name="add_new_prod_in_catalog">add</button>
+            <button id="close_products">X</button>
+              <?php
+              for ($i=1; $i < 7; $i++) {
+                echo "<div class='gallery_img' id='open_image'>
+                        <input type='checkbox' name=new_prod_int_catalog>
+                        <img src='source/d".$i.".jpg'>
+                          <h4>Red Dress</h4>
+                          <p>$124.99</p>
+                      </div>";
+              }
+              ?>
+        </div>
       <script>
 
       $("#open_image").click(function(){
         $(".prod_list_to_add").show();
+        jQuery('.prod_list_to_add').css('opacity', '1');
+        $(".black_div").show();
+      });
+      $("#close_products").click(function(){
+        $(".prod_list_to_add").hide();
+        $(".black_div").hide();
       });
 
       </script>
