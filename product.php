fa9f80b27fdd86bac499d99fb978a8efc3556b7c
FeRla17X <ucha1bokeria@gmail.com>
2020-10-07 10:42:17 -0700

    night mode demo on product

--- product.php
+++ product.php
@@ -125,27 +125,20 @@
       });
       // NIGHT MODE
       function nightMode(){
-        $('#body').toggleClass('night');
         $('#logo').attr('src', function(index,attr) {return attr=='https://rb.gy/mkquhl' ?
           'https://www.wallpaperflare.com/static/948/854/435/mr-robot-e-corp-evil-corp-evil-wallpaper.jpg':'https://rb.gy/mkquhl';
         });
-
         $('.Header').toggleClass('night');
         $('.line').toggleClass('white');
         $('.lang').toggleClass('night');
         $('.more-lang').toggleClass('night-lang');
         $('#moon').toggleClass('white-moon');
 
-        $('.poster-text').toggleClass('glow');
-        $('.Poster').toggleClass("white");
-        $('.Catalog').toggleClass("night");
-        $('.Location').toggleClass("night");
-
-        $('.Contact').toggleClass('white-contact');
-        $('.Contact').toggleClass("white");
-
-        $('.About').toggleClass("night");
-        $('.Footer').toggleClass("white");
+        $('.search-product').children('input').toggleClass('night');
+        $('.search-product').toggleClass('night');
+        $('.gallery_img').toggleClass('night');
+        $('body').toggleClass('night-product');
+        $('.pagination').toggleClass('night');
       }
 
       // HOVER POSTER BANNER
