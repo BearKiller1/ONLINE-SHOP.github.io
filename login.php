4577a2802d1d64cfd0860efabe6c20284e02ecda
UniqueDesigns-UI <dachi.xucishvili8@gmail.com>
2020-10-12 20:21:11 +0400

    Log in page/set up

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Log in</title>
+    <link rel="stylesheet" href="style/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <body>
+    <form action="login.php" class="login_form">
+
+        <img src="source/log-in.png" class="login_icon">
+      <div class="form_inputs">
+        <img src="source/user.png" class="user">
+        <input class="input-field" type="text" placeholder="მომხმარებლის სახელი" name="usrnm">
+        <img src="source/lock-icon.png" class="pwd">
+        <input class="input-field" type="text" placeholder="მომხმარებლის პაროლი" name="usrnm">
+      </div>
+      <button type="button" name="button" class="login_button w3-blue">შესვლა</button>
+      <a href="#" class="forget_pwd">პაროლის აღდგენა</a>
+    </form>
+  </body>
+</html>
