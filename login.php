219e81059428cf109cc5568995ba64830b9468ec
UniqueDesigns-UI <dachi.xucishvili8@gmail.com>
2020-10-14 22:32:36 +0400

    updating Log in page/form

--- login.php
+++ login.php
@@ -5,14 +5,14 @@
     <title>Log in</title>
     <link rel="stylesheet" href="style/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <body>
     <form action="login.php" class="login_form">
-
         <img src="source/log-in.png" class="login_icon">
       <div class="form_inputs">
-        <img src="source/user.png" class="user">
+        <img src="source/tmp0.png" class="user">
         <input class="input-field" type="text" placeholder="მომხმარებლის სახელი" name="usrnm">
-        <img src="source/lock-icon.png" class="pwd">
+        <img src="source/tmp.png" class="pwd">
         <input class="input-field" type="text" placeholder="მომხმარებლის პაროლი" name="usrnm">
       </div>
       <button type="button" name="button" class="login_button w3-blue">შესვლა</button>
